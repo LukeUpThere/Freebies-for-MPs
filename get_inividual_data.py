fa44809afe7f6a42167ce450f0baec9d875e84a4
Luke <grayguy.101@gmail.com>
2023-01-14 20:52:29 +0000

    Adding MP object dict and Win compatibility
    - Added functionality to get_individual_data.py to create a dictionary of MP objects containing donations, party and constituency (by referencing a 3rd party CSV).
    - Made minor changes to get_link_dic.py to make it Windows compatible.
    - Removed outdated .pydata file.

--- get_inividual_data.py
+++ get_inividual_data.py
@@ -1,20 +1,21 @@
 # Gets financial data on each MP
 import os
 import pickle
+import csv
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import re
 from dateutil.parser import parse
 ### CLASSES ###
 
-class MemberOfParlement:
-    def __init__(self, name, constituency, party):
+class MP:
+    def __init__(self, name, constituency = 'Unknown', party = 'Unknown'):
         self.name = name
         self.constituency = constituency
         self.party = party
         self.donations = []
 
-    def add_donation(self, amount, source, date):
+    def add_donation(self, amount, interest_type, date):
         self.donations.append({"amount": amount,
                                "interest type": interest_type, 
                                "date": date})
@@ -55,9 +56,11 @@ def get_total_from_monthly(text):
                                 text[start_date_match.end():text.find('£')])
     if end_date_match:
         end_date = parse(end_date_match.group(1))
+        date_received = end_date_match
         #print('end date matched.') # Debugging
     else:
         end_date = parse("01 May 2022")
+        date_received = "01 May 2022"
         
     written_sd = parse(start_date_match.group(1))
     session_sd = parse("01 May 2021")
@@ -71,21 +74,21 @@ def get_total_from_monthly(text):
         #print(f"income: {income}") # Debugging
     else:
         # handle the case where no match was found
-        return 'error with income_match'
+        return ('error with income_match', date_received)
 
     income = float(income_match.group(1).replace(',', '')[1:])
 
     # Calculate the total income
     total_income = round(income * ((end_date - start_date).days / 30), 2)
-    return total_income
+    return (total_income, date_received)
 
-def get_freebies(name, url):
+def webscrape_freebies(name, url):
     """
     Scrapes a webpage for financial interests of a member of parliament and 
-    returns the total amount of money received.
+    returns details about each donation in the form of a list of dictionaries.
     :param name: name of the member of parliament
     :param url: url of the webpage to scrape
-    :return: total monetary amount received
+    :return: list of donations received
     """
     # Correctly set up the Chrome Driver Exe path.
     os.environ["PATH"] += os.pathsep + 'D:\Code\chromedriver_win32'
@@ -103,6 +106,8 @@ def get_freebies(name, url):
     print('\n' + name)
     interest_type = ''
     for info in infos:
+        print(info)
+        amount = 0
         text = info.text
         tl = text.lower()
         # Print numbered headers.
@@ -113,6 +118,7 @@ def get_freebies(name, url):
             else:
                 print(text)
                 interest_type = text
+            continue
         # Locate and print stated totals.
         elif 'total' in text and text[text.find("total"):].find('£') != -1:
             tot_indx = text.find("total")
@@ -122,15 +128,16 @@ def get_freebies(name, url):
             tot = text[tot_indx + find_p + 1:tot_indx + find_p + end_indx]
             total = ''.join([c for c in tot if c in '1234567890.']).strip('.')
             print(f"£_{total_value} (Suspected total)") # Printing
+            date_received = re.search(r"(\d{1,2} [A-Za-z]{3,9} \d{4})", text)
             amount = total_value
         # Locate date ranges with monthly pay and calculate an annual total.
-        elif all(x in tl for x in ['from','until','£']) and not 
-             any(x in tl for x in ['annual', 'yearly', 'a year']):
-            total_value = get_total_from_monthly(text)
+        elif all(x in tl for x in ['from','until','£']) and not any(x in tl for x in ['annual', 'yearly', 'a year']):
+            total_value, date_received = get_total_from_monthly(text)
             print(f"£_{total_value} (Calculated total)") # Printing
             amount = total_value
         # Locate all other monetary sums and print them.
         else:
+            date_received = re.search(r"(\d{1,2} [A-Za-z]{3,9} \d{4})", text)
             words_in_info = info.text.split(' ')
             #print(info.text) # Debugging
             for word in words_in_info:
@@ -138,25 +145,53 @@ def get_freebies(name, url):
                     total_value = ''.join([c for c in word if c in 
                                                     '1234567890.']).strip('.')
                     print(f"£_{total_value}") # Printing
-                    amount += total_value
+                    amount += float(total_value)
         
-        donations.append({'amount': amount,
-                          'interest type': interest_type,
-                          'date': date_received})
+        if amount:
+            donations.append({'amount': amount,
+                              'interest type': interest_type,
+                              'date': date_received.group(1)})
     return donations
 
 ### MAIN CODE ###
 
 # Pickle load links
 mp_finances_link_dic = {}
-with open('mp_finances_link_dic_v2.pydata', 'rb') as f:
+with open('mp_finances_link_dic.pydata', 'rb') as f:
     mp_finances_link_dic = pickle.load(f)
 
-mp_finances_dic = {}
-# View items in dictionary
+# Load CSV and get party and constituency data
+fields = []
+mp_party_constit = {}
+with open('mps.csv', 'r') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    feilds = next(csv_reader)
+    rows = [row for row in csv_reader]
+for row in rows:
+    mp_fl_names = (row[2], row[1])
+    mp_party_constit[mp_fl_names] = {'Party': row[3], 'Constituency': row[4]}
+
+
+# Create a dictionary of MP objects, match the names to the CSV data and apply
+#   that data to each object (add each MPs Party and Constituency).
+mps = {}
 for name, link in mp_finances_link_dic.items():
-    if name == 'Aiken, Nickie ' or name == 'Abbott, Ms Diane ':
-        mp_finances_dic[name] = get_freebies(name, link)
+    if name[0] == 'A':
+        for names, detail in mp_party_constit.items():
+            if all(x in name for x in names):
+                mps[name] = MP(name, detail['Constituency'], detail['Party'])
+                print(f'{name} vs {names}: csv match')
+                break
+            else:
+                mps[name] = MP(name)
+                print(f'{name} vs {names}: no csv match')
+        # Add donations to each MP
+        donations = webscrape_freebies(name, link)
+        for donation in donations:
+            mps[name].add_donation(donation['amount'], 
+                                   donation['interest type'],
+                                   donation['date'])
+        print(mps[name].party)
 
 # Pickle the dictionary
 # ~ file_object = open('mp_finances_dic.pydata', 'wb')
--- get_link_dic.py
+++ get_link_dic.py
@@ -2,28 +2,25 @@ import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import pickle
-os.system('clear')
 
 # URL of the webpage containing the links to the individual pages
 url = 'https://publications.parliament.uk/pa/cm/cmregmem/220503/contents.htm'
 base_url = 'https://publications.parliament.uk/pa/cm/cmregmem/220503/'
 
-# Create a Safari webdriver, Navigate to the webpage
-driver = webdriver.Safari()
+# Correctly set up the Chrome Driver Exe path.
+os.environ["PATH"] += os.pathsep + 'D:\Code\chromedriver_win32'
+# Use a chrome webdriver to get the HTML from the URL and make some Soup.
+driver = webdriver.Chrome()
 driver.get(url)
 driver.implicitly_wait(10)
-
-# Extract the HTML content of the webpage and make soup
-html_content = driver.page_source
-soup = BeautifulSoup(html_content, 'html.parser')
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 # Initialise variables to store the names and links of the individuals
 names = []
 links = []
 
-# Find the links to the individual pages
+# Find the links to the individual pages and iterate over them
 link_tags = soup.find_all('a')
-# Iterate over the link tags
 for link_tag in link_tags:
     # Filter then extract the name of the individual from the tag
     if len(link_tag.text) > 0 and link_tag.text[-1] == ' ':
@@ -33,17 +30,12 @@ for link_tag in link_tags:
         if '.htm' in link_tag['href']  and '/' not in link_tag['href']:
             links.append(base_url + link_tag['href'])
 
-mp_finances_link_dic = {}
 # Add the names and links of the individuals to a dictionary
+mp_finances_link_dic = {}
 for i in range(len(names)):
     mp_finances_link_dic[names[i]] = links[i]
-    # ~ print(f'{names[i]}: {links[i]}') # Debugging
 
 # Pickle the dictionary
-file_object = open('mp_finances_link_dic_v2.pydata', 'wb')
+file_object = open('mp_finances_link_dic.pydata', 'wb')
 pickle.dump(mp_finances_link_dic, file_object)
 file_object.close()
-
-
-# Print the parsed HTML content
-#print(soup.prettify())
